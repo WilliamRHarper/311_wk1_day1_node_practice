6e315ee5a3bfea6459aa265e8744597c5be0ef6a
WilliamRHarper <Wrharpe1230@gmail.com>
2020-07-07 11:25:52 -0500

    passed all tests

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,15 +1,25 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  return num % 2 === 0? true:false;
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  return arr.reduce(reducer);
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
+  for (let i = 0; i < arr.length; i++){
+    for (let j = 1; j < arr.length; j++){
+      if (arr[i] + arr[j] === sum) {
+        return true;
+      }else {
+        return false;
+      }
+    }
+  }
 
 }
 
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,23 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  return str.split(delim);
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
-
-}
+  let pair = [];
+  for (let i = 0; i < str.length; i+=2){
+    pair.push(str[i] + str[i + 1]);
+  }
+  return pair;
+  }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  let splitString = str.split('');
+  let reversed = splitString.reverse();
+  let reversedString = reversed.join('');
+  return reversedString;
 }
 
 module.exports = {
